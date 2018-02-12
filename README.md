1a9399d2cc98efbf681f83fd668792e4ca6f194e
clement-bey-cgi <clement.bey@ipilyon.net>
2018-02-12 23:17:56 +0100

    1er Commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # jeu2D
 Little 2D game in JAVA, without framework
+Premier Commit de test
---
+++ src/jeu2D/ExampleClass.java
@@ -0,0 +1,10 @@
+package jeu2D;
+
+public class ExampleClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bite");
+	}
+
+}
