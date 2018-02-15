5dc07359f36c9a61c3e6c06918516b4b1a05ddb4
Clement Bey <clement.bey@ipilyon.net>
2018-02-15 09:33:12 +0100

    commit

---
+++ src/jeu2D/Main.java
@@ -0,0 +1,13 @@
+package jeu2D;
+
+import javax.swing.JFrame;
+import jeu2D.config.MaFenetre;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("XD");
+		
+		MaFenetre maFenetre = new MaFenetre();
+	}
+}
---
+++ src/jeu2D/config/MaFenetre.java
@@ -0,0 +1,14 @@
+package jeu2D.config;
+
+import javax.swing.JFrame;
+
+public class MaFenetre extends JFrame {
+	
+	public MaFenetre() {
+		this.setVisible(true);
+		this.setSize(800,800);
+		this.setLocationRelativeTo(null);
+		this.setTitle("Jeu 2D");
+	}
+	
+}
